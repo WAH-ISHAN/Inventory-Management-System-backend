54d8a28012eb8281765057b1055f598075c27109
Wah-Ishan <wahasinduishan@gmail.com>
2026-04-26 02:10:38 +0530

    implement AuthController with login, logout, and user registration functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,9 @@ class AuthController extends Controller{
 
         return response()->json([
             'access_token' => $token,
+            'token' => $token,
             'token_type' => 'Bearer',
+            'user' => $user
         ]);
     }
 
