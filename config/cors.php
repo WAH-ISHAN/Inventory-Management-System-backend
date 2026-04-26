d8ac01257885375cc214f0c0a96133b0902a255b
Wah-Ishan <wahasinduishan@gmail.com>
2026-04-26 10:10:57 +0530

    fix: add additional frontend URL to CORS allowed origins and define patterns

--- config/cors.php
+++ config/cors.php
@@ -10,10 +10,13 @@ return [
         'http://localhost:3000',
         'https://inventory-management-system-fronten-gilt.vercel.app',
         'https://inventory-management-system-frontend-4y8mmbzm8.vercel.app',
+        'https://inventory-management-system-frontend-pgp167eb1.vercel.app',
         env('FRONTEND_URL', 'http://localhost:3000')
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '^https:\/\/inventory-management-system-frontend-[a-z0-9-]+\.vercel\.app$',
+    ],
 
     'allowed_headers' => ['*'],
 
