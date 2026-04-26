82e4d0148014c597ad6a53206b1eb8c7d8b032ff
Wah-Ishan <wahasinduishan@gmail.com>
2026-04-26 10:59:11 +0530

    fix: implement response in index method of PlaceController

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -11,7 +11,7 @@ class PlaceController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(\App\Models\Place::with('cupboard')->get());
     }
 
     /**
